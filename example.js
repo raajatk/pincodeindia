fc21839b64dcba3768417a8e473b52141df4dae8
Rajat Katiyar <rajatkatiyar2009@gmail.com>
2017-12-19 16:42:14 +0530

    added an example file

---
+++ example.js
@@ -0,0 +1,10 @@
+var pincodeSearch = require('./index.js');
+var async = require('async');
+
+async.auto({
+  getPinInfo:function(next){
+    pincodeSearch(208024,next);
+  }
+},function(err,results){
+  console.log(results.getPinInfo);
+})
