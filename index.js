c29e87076bfc2f1065468b569c3af937400a1abc
Rajat Katiyar <rajatkatiyar2009@gmail.com>
2017-12-19 15:33:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = function(pincode, callback) {
+
+  const file = './info.json';
+  const fs = require('fs');
+  const async = require('async');
+  var pincode = pincode.toString();
+
+  async.auto({
+    readFile:function(next){
+      fs.readFile(file,'utf8',function(err,data){
+        var obj=JSON.parse(data);
+        next(null,obj);
+      });
+    },
+    searchPin:['readFile',function(result, next){
+      var pinArray = result.readFile;
+      var resObj = [];
+      async.forEach(pinArray,function(pinObj, arrNext){
+        if(pincode == pinObj.pincode){
+          resObj.push(pinObj);
+        }
+        arrNext();
+      },function(err){
+        next(null,resObj);
+      })
+    }]
+  },function(err, results){
+    if(results.searchPin.length>0){
+      var resObj = {
+        error:err,
+        result:results.searchPin,
+        message:"The Data has been fetched Successfully."
+      }
+      callback(null,resObj);
+    }else {
+      var resObj = {
+        error:err,
+        result:results.searchPin,
+        message:"Sorry! No data available for the queried PinCode."
+      }
+      callback(null,resObj);
+    }    
+  })
+};
+
+// module.exports = 'hello';
